c3144c332f56d1ab9235f28ca38e95a02b1580b6
heyimmatt <mrmcfarland@gmail.com>
2020-07-24 11:14:11 -0700

    Add currency code validation function and test

--- forex.py
+++ forex.py
@@ -3,6 +3,15 @@ from forex_python.converter import CurrencyRates, CurrencyCodes
 cr = CurrencyRates()
 cc = CurrencyCodes()
 
+def validate_codes(from_code, to_code):
+    from_name = cc.get_currency_name(from_code)
+    to_name = cc.get_currency_name(to_code)
+    
+    if from_name == None or to_name == None:
+        return False
+
+    return True
+
 def get_rate(from_code, to_code, amount):
     try: 
         rate = "{:.2f}".format(cr.convert(from_code, to_code, amount))
--- test_forex.py
+++ test_forex.py
@@ -1,11 +1,16 @@
 from unittest import TestCase
-from forex import get_rate
+from forex import get_rate, validate_codes
 
 class ForexFunctionsTests(TestCase):
 
+    def test_code_validation(self):
+        valid_code = validate_codes('USD', 'EUR')
+        invalid_code = validate_codes('ZZZ', 'EUR')
+
+        self.assertTrue(valid_code)
+        self.assertFalse(invalid_code)
+
     def test_conversion(self):
         rate = get_rate('USD', 'EUR', 1)
-
-        print(rate)
 
         self.assertIn('€', rate)
