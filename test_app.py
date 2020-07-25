8a1d2d910249728dc2f2abe2d01ff390f6dbdbc5
heyimmatt <mrmcfarland@gmail.com>
2020-07-24 23:09:55 -0700

    Update test_home_post_route to post form data and redirect url to be assertIn

--- test_app.py
+++ test_app.py
@@ -8,6 +8,7 @@ app.config['TESTING'] = True
 # This is a bit of hack, but don't use Flask DebugToolbar
 app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
 
+
 class AppViewsTests(TestCase):
 
     def test_home_get_route(self):
@@ -16,15 +17,19 @@ class AppViewsTests(TestCase):
             html = resp.get_data(as_text=True)
 
             self.assertEqual(resp.status_code, 200)
-            self.assertIn('<form class="mb-3" id="currency-form" action="/" method="POST">', html)
-    
+            self.assertIn(
+                '<form class="mb-3" id="currency-form" action="/" method="POST">', html)
+
     def test_home_post_route(self):
         with app.test_client() as c:
-            resp = c.post('/')
-
+            resp = c.post('/', data={
+                'from-code': 'USD',
+                'to-code': 'EUR',
+                'amount': '1'
+            })
             self.assertEqual(resp.status_code, 302)
-            self.assertEqual(resp.location, "http://localhost/rate")
-  
+            self.assertIn('http://localhost/rate?exchange_rate', resp.location)
+
     def test_rate_get_route(self):
         with app.test_client() as c:
             resp = c.get('/rate')
